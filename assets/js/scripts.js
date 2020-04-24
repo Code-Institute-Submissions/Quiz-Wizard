f12b1633a38191b23187d590140dca8c205931ff
James Fractal <james.fractal@protonmail.com>
2020-04-25 08:54:19 +1000

    combine all answers into one array and display them in each answer box

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,5 +5,14 @@ $(document).ready(function () {
     .then(data => {
         let triviaData = data.results;
         document.getElementById('triviaQuestion').innerHTML = triviaData[0].question;
+        triviaData[0].incorrect_answers.push(triviaData[0].correct_answer);
+        // console.log(triviaData[0]);
+        // console.log(triviaData[0].category);
+        triviaData[0].incorrect_answers.forEach(answer => {
+            for (i = 0; i < triviaData[0].incorrect_answers.length; i++) {
+                document.getElementById('triviaAnswer' + (i + 1)).innerHTML = triviaData[0].incorrect_answers[i]
+                console.log(i);
+            }
+        });
     })
 });
