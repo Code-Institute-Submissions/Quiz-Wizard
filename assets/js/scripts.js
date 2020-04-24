341ffd11f82ba526cb8a6d361def7272d4718770
James Fractal <james.fractal@protonmail.com>
2020-04-24 14:30:34 +1000

    add javascript to display question from data in question box

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     
     fetch('https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple')
     .then(res => res.json())
-    .then(data => console.log(data))
-    
+    .then(data => {
+        let triviaData = data.results;
+        document.getElementById('triviaQuestion').innerHTML = triviaData[0].question;
+    })
 });
