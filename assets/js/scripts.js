1bcabb228678fc6ade6ec1e1f7ead7ab7b86baf3
James Fractal <james.fractal@protonmail.com>
2020-04-25 11:02:22 +1000

    add class to p and target with click event handler, check for correct or incorrect answer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
     .then(res => res.json())
     .then(data => {
         let triviaData = data.results;
+        let correctAnswer = triviaData[0].correct_answer;
+        console.log(correctAnswer);
         document.getElementById('triviaQuestion').innerHTML = triviaData[0].question;
         triviaData[0].incorrect_answers.push(triviaData[0].correct_answer);
         // console.log(triviaData[0]);
@@ -11,8 +13,20 @@ $(document).ready(function () {
         triviaData[0].incorrect_answers.forEach(answer => {
             for (i = 0; i < triviaData[0].incorrect_answers.length; i++) {
                 document.getElementById('triviaAnswer' + (i + 1)).innerHTML = triviaData[0].incorrect_answers[i]
-                console.log(i);
+                // console.log(i);
             }
         });
-    })
+
+        $('.game--answer--single').click(function (e) {
+            e.preventDefault();
+            // console.log('click');
+            console.log($(this).text());
+            // console.log('----');
+            if ($(this).text() == correctAnswer) {
+                console.log('correct');
+            } else {
+                console.log('incorrect');
+            }    
+        });
+    });
 });
