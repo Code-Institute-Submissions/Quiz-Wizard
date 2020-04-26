ed74373fe75a86f22b6ab30259ff331f738d5460
James Fractal <james.fractal@protonmail.com>
2020-04-26 15:39:18 +1000

    add click handler for result of click. update id and object key categories to match

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
         'books': 'category=10',
         'film': 'category=11',
         'television': 'category=14',
-        'videoGames': 'category=15',
-        'scienceNature': 'category=17',
+        'video-games': 'category=15',
+        'science-nature': 'category=17',
         'sport': 'category=21',
         'history': 'category=23',
         'politics': 'category=24',
@@ -29,73 +29,24 @@ $(document).ready(function () {
         'vehicles': 'category=28',
         'music': 'category=12',
         'celebrities': 'category=26',
-        'generalKnowledge': 'category=9',
+        'general-knowledge': 'category=9',
     })[categoryChoice]
     
     // console.log(categoryURL('film'));
 
-    $('.category').click(function() {
+    $('.category').click(function(event) {
         // console.log($(this).attr('id'));
-        console.log(categoryURL($(this).attr('id')));
+        // console.log(categoryURL($(this).attr('id')));
+        return categoryURL($(this).attr('id'))
     })
 
-    
+    $('.category').click(function(event) {
+        urlCategory = event.result
+        console.log(urlCategory);
+    })
 
     // $('.category').click(function(e) {
     //     e.preventDefault();
-    //     /* Category selection switch */
-    //     switch ($(this).prop('id')) {
-    //         case 'mythology':
-    //         console.log('mythology');
-    //         break;
-    //         case 'books':
-    //         console.log('books');
-    //         break;
-    //         case 'film':
-    //         console.log('film');
-    //         break;
-    //         case 'television':
-    //         console.log('television');
-    //         break;
-    //         case 'videoGames':
-    //         console.log('video games');
-    //         break;
-    //         case 'scienceNature':
-    //         console.log('Science and nature');
-    //         break;
-    //         case 'sports':
-    //         console.log('sports');
-    //         break;
-    //         case 'history':
-    //         console.log('history');
-    //         break;
-    //         case 'politics':
-    //         console.log('politics');
-    //         break;
-    //         case 'animals':
-    //         console.log('animals');
-    //         break;
-    //         case 'art':
-    //         console.log('art');
-    //         break;
-    //         case 'geography':
-    //         console.log('geography');
-    //         break;
-    //         case 'vehicles':
-    //         console.log('vehicles');
-    //         break;
-    //         case 'music':
-    //         console.log('music');
-    //         break;
-    //         case 'celebrities':
-    //         console.log('celebrities');
-    //         break;
-    //         case 'generalKnowledge':
-    //         console.log('general knowledge');
-    //         break;
-    //         default:
-    //         console.log('default');
-    //     }
         
     //     $('.game--categories').addClass('game--panel__hidden').removeClass('game--panel__shown');
     //     $('.game--difficulty').addClass('game--panel__shown').removeClass('game--panel__hidden');
@@ -110,65 +61,69 @@ $(document).ready(function () {
 
     triviaURL = urlStart + url10 + urlCategory + urlEasy + urlType
 
-    fetch(triviaURL)
-    .then(res => res.json())
-    .then(data => {
-        let triviaData = data.results;
-        let triviaQuestions = []
-        let triviaAnswers = []
-        let triviaCorrect = []
-        // push all questions into array
-        triviaData.forEach((question, index) => {
-            triviaQuestions.push(triviaData[index].question)
+    function getTriviaData(playerSelection) {
+        fetch(triviaURL)
+        .then(res => res.json())
+        .then(data => {
+            let triviaData = data.results;
+            let triviaQuestions = []
+            let triviaAnswers = []
+            let triviaCorrect = []
+            // push all questions into array
+            triviaData.forEach((question, index) => {
+                triviaQuestions.push(triviaData[index].question)
+            });
+            // push correct answer into incorrect answers
+            triviaData.forEach((answer, index) => {
+                triviaData[index].incorrect_answers.push(triviaData[index].correct_answer)
+            });
+            // push all answer choices into array
+            triviaData.forEach((allAnswers, index) => {
+                triviaAnswers.push(triviaData[index].incorrect_answers)
+            })
+            // shuffle answer array
+            for (i =0; i < triviaAnswers.length; i++) {
+                // console.log(triviaAnswers[i]);
+                triviaAnswers[i].sort(() => Math.random() - 0.5);
+                // console.log(triviaAnswers[i]);
+            }
+            // push all correct answers into array
+            triviaData.forEach((cAnswer, index) => {
+                triviaCorrect.push(triviaData[index].correct_answer)
+            })
+            // iterate array to display questions
+            triviaQuestions.forEach((question, index) => {
+                document.getElementById(`trivia${index}Question`).innerHTML = triviaQuestions[index];
+            })
+            // iterate array to display answers
+            triviaAnswers.forEach((answerArray, index1) => {
+                    triviaAnswers[index1].forEach((answer, index2) => {
+                        document.getElementById(`trivia${index1}Answer${index2}`).innerHTML = triviaAnswers[index1][index2]
+                    })
+            })
+    
+            // console.log(triviaQuestions);
+            // console.log(triviaAnswers);
+            // triviaData[0].incorrect_answers.forEach(answer => {
+            //     for (i = 0; i < triviaData[0].incorrect_answers.length; i++) {
+            //         document.getElementById('triviaAnswer' + (i + 1)).innerHTML = triviaData[0].incorrect_answers[i]
+            //         console.log(i);
+            //     }
+            // });
+    
+            $('.game--answer--single').click(function (e) {
+                e.preventDefault();
+                // console.log('click');
+                console.log($(this).text());
+                // console.log('----');
+                if ($(this).text() == correctAnswer) {
+                    console.log('correct');
+                } else {
+                    console.log('incorrect');
+                }
+            });
         });
-        // push correct answer into incorrect answers
-        triviaData.forEach((answer, index) => {
-            triviaData[index].incorrect_answers.push(triviaData[index].correct_answer)
-        });
-        // push all answer choices into array
-        triviaData.forEach((allAnswers, index) => {
-            triviaAnswers.push(triviaData[index].incorrect_answers)
-        })
-        // shuffle answer array
-        for (i =0; i < triviaAnswers.length; i++) {
-            // console.log(triviaAnswers[i]);
-            triviaAnswers[i].sort(() => Math.random() - 0.5);
-            // console.log(triviaAnswers[i]);
-        }
-        // push all correct answers into array
-        triviaData.forEach((cAnswer, index) => {
-            triviaCorrect.push(triviaData[index].correct_answer)
-        })
-        // iterate array to display questions
-        triviaQuestions.forEach((question, index) => {
-            document.getElementById(`trivia${index}Question`).innerHTML = triviaQuestions[index];
-        })
-        // iterate array to display answers
-        triviaAnswers.forEach((answerArray, index1) => {
-                triviaAnswers[index1].forEach((answer, index2) => {
-                    document.getElementById(`trivia${index1}Answer${index2}`).innerHTML = triviaAnswers[index1][index2]
-                })
-        })
+    }
 
-        // console.log(triviaQuestions);
-        // console.log(triviaAnswers);
-        // triviaData[0].incorrect_answers.forEach(answer => {
-        //     for (i = 0; i < triviaData[0].incorrect_answers.length; i++) {
-        //         document.getElementById('triviaAnswer' + (i + 1)).innerHTML = triviaData[0].incorrect_answers[i]
-        //         console.log(i);
-        //     }
-        // });
-
-        $('.game--answer--single').click(function (e) {
-            e.preventDefault();
-            // console.log('click');
-            console.log($(this).text());
-            // console.log('----');
-            if ($(this).text() == correctAnswer) {
-                console.log('correct');
-            } else {
-                console.log('incorrect');
-            }    
-        });
-    });
+    
 });
